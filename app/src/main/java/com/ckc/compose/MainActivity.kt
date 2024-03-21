7dd8c0aea8d542466d6796dfce0ab161ced5ab82
KiranChalasani <kchalasa@uab.edu>
2024-03-20 22:30:58 -0500

    Added a constarint that includes button and text

--- app/src/main/java/com/ckc/compose/MainActivity.kt
+++ app/src/main/java/com/ckc/compose/MainActivity.kt
@@ -4,16 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.ckc.compose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    SampleText("hero")
+                    CreateConstraintLayout()
                 }
             }
         }
@@ -34,17 +32,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SampleText(value: String) {
-    Text(text = value,
-        fontSize = 18.sp,
-        color = Color.Red,
-        modifier = Modifier
-            .fillMaxSize() // This will make the parent container fill the available space
-            .wrapContentSize(Alignment.Center)) // This will center the content within the parent container)
+fun CreateConstraintLayout() {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        val (button, text) = createRefs()
+
+        Button(
+            onClick = { /* Handle click */ },
+            modifier = Modifier.constrainAs(button) {
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }
+        ) {
+            Text("Centered Button")
+        }
+
+        Text(
+            "Centered Text",
+            modifier = Modifier.constrainAs(text) {
+                top.linkTo(button.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            }
+        )
+    }
 }
 
 @Preview
 @Composable
-fun SampleTextPreview() {
-    SampleText(value = "Hello")
+fun SampleConstraintLayoutPreview() {
+    CreateConstraintLayout()
 }
